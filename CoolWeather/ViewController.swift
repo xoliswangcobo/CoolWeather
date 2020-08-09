cf1d838c329ba5fd83e162e5e1b9babc2dde261e
Xoliswa Ngcobo <xoliswangcobo@icloud.com>
2020-08-09 19:56:13 +0200

    Initial Commit

---
+++ CoolWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoolWeather
+//
+//  Created by Xoliswa on 2020/08/09.
+//  Copyright © 2020 Xoliswa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
