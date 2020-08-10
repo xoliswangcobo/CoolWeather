3718e07e30525ae4bd726893089155df22e2650e
Xoliswa Ngcobo <xoliswangcobo@icloud.com>
2020-08-10 19:23:31 +0200

    Extension added.

---
+++ CoolWeather/Extensions/UIView+DropShadow.swift
@@ -0,0 +1,19 @@
+//
+//  UIView+DropShadow.swift
+//  Swift+Extensions
+//
+//  Created by Xoliswa on 2020/08/10.
+//  Copyright © 2020 Xoliswa. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func addDropShadowToView() {
+        self.layer.masksToBounds =  false
+        self.layer.shadowColor = UIColor.darkGray.cgColor
+        self.layer.shadowOffset = CGSize(width: 2.0, height: 2.0)
+        self.layer.shadowOpacity = 1.0
+    }
+}
